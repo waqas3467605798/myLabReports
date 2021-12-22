1fbe6a3b27be703f6aa4e4502a702b9be6af5d0e
waqas saleem <waqas.mba86@gmail.com>
2021-12-22 22:19:33 +0500

    working on get Reports of customer

--- src/components/GetReport.js
+++ src/components/GetReport.js
@@ -12,7 +12,10 @@ import {Link, Route, BrowserRouter} from 'react-router-dom'
       this.state ={
               user:null,
               userEmail:null,
-              customerReports:[]
+              customerReports:[],
+              loadCustomerList:false,
+              refreshList:false,
+              getReportObject:{}
       }
 
   }
@@ -23,23 +26,39 @@ import {Link, Route, BrowserRouter} from 'react-router-dom'
     var userId = firebase.auth().currentUser.uid;
     var userEmail = firebase.auth().currentUser.email
     this.setState({user:userId,userEmail:userEmail})
-    res()
+
+      var list = []
+
+    firebase.database().ref('customerReports').on('child_added' , (data)=> { 
+      list.push(data.val())
+    }  )
+
+
+    res(list)
     rej('Operation Failed: Data From Firebase does not push in state successfully')
   } )
-  dataPushPromise.then(()=>{
+  dataPushPromise.then((customerObj)=>{
   
-    firebase.database().ref('customerReports').on('child_added' , (data)=> { 
-      this.state.customerReports.push(data.val())
-    }  )
+    this.setState({customerReports:customerObj.sort((a, b) => (a.reportNumber < b.reportNumber) ? 1 : -1), loadCustomerList:true})
   
   
   })
   
   }
 
-
+  refreshList=()=>{
+    this.setState({refreshList: !this.state.refreshList})
+  }
 
   
+  getReport=()=>{
+    // var reportNo = document.getElementById('repNo').value
+  
+    // var ourObj = this.state.customerReports.find((obj)=>{return obj.reportNumber === reportNo})
+
+    // console.log(ourObj)
+  }
+
 
     render(){
         return(
@@ -47,13 +66,21 @@ import {Link, Route, BrowserRouter} from 'react-router-dom'
         <span style={{fontSize:'12px'}}><b style={{color:'green',marginLeft:'30px'}}>{this.state.userEmail}</b> / {navigator.onLine===true ? <span style={{color:'green'}}>You are online</span> : <span style={{color:'red'}}>You are OffLine</span>}</span>
 <br/> <br/>
 
-
+          {/* Div of to getting Customer Report */}
           <div className='container'>
+          <span style={{fontSize:'19px',color:'blue'}}>Please Enter Report Number</span><br/><br/>
+          <input type='text' id='repNo' placeholder='Enter Report Number Here'/> <br/>
+          <button style={{padding:'3px',fontSize:'14px',borderRadius:'4px', color:'blue', backgroundColor:'lightgreen'}} onClick={this.getReport}>Get Report</button>  
+          </div>
 
+<br/><br/><br/>
 
-        <table className='browser-default'><thead><tr><th>R#</th><th>Date</th><th>Name</th><th>Age</th><th>Test Name</th></tr></thead><tbody>{this.state.customerReports.map((it,ind)=>{return <tr key={ind}><td>{it.reportNumber}</td><td>{it.date}</td><td>{it.patientName}</td><td>{it.age}</td><td>{it.patientReport.map((item,index)=>{return <span key={index}>{item.testName}</span>})}</td></tr>})}</tbody></table>
-
-
+          {/* Div of List of all customer Reports */}
+          <div className='container'>
+          <button style={{padding:'3px',fontSize:'14px',borderRadius:'4px', color:'blue', backgroundColor:'lightgreen'}} onClick={this.refreshList}>Show List</button>  
+        <div className={this.state.loadCustomerList===false?'display' : ''}>
+        <table className='browser-default'><thead><tr style={{backgroundColor:'lightyellow'}}><th>R#</th><th>Date</th><th>Name</th><th>Age</th><th>Test Name</th></tr></thead><tbody>{this.state.customerReports.map((it,ind)=>{return <tr key={ind}><td>{it.reportNumber}</td><td>{it.date}</td><td>{it.patientName}</td><td>{it.age}</td><td>{it.patientReport.map((item,index)=>{return <span key={index}>{item.testName} , </span>})}</td></tr>})}</tbody></table>
+        </div>
           </div>
 
           </div>
